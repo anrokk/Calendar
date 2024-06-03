5a22f8ea4a06967aff2d4dc7911e9d20e34f3d55
anrokk <anrokk@taltech.ee>
2024-06-03 22:55:46 +0300

    test

---
+++ src/script.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    generateCalendar();
+};
+function generateCalendar() {
+    const calendar = document.getElementById('calendar');
+    const currentDate = new Date();
+    const month = currentDate.getMonth();
+    const year = currentDate.getFullYear();
+
+    const firstDayOfMonth = new Date(year, month, 1);
+    const lastDayOfMonth = new Date(year, month + 1, 0);
+
+    const firstDayOfWeek = firstDayOfMonth.getDay();
+}
